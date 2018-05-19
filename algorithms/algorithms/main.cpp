18d989f064d0e35ed4c9cb4cf6d6904665472088
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2018-05-19 23:44:06 +0300

    added system of linear equations calculator

--- algorithms/algorithms/main.cpp
+++ algorithms/algorithms/main.cpp
@@ -37,6 +37,45 @@ void binary_insertion_sort(int arr[], int len)
 	}
 }
 
+void linear_equation_system()
+{
+	const int rows = 3;
+	const int cols = 4;
+	double equation[rows][cols] =
+	{
+		{ 2, 1, 0, 134 },
+		{ 5, -3, 4, 12 },
+		{ 8, 9, 5, -36 }
+	};
+
+	for (size_t row = 0; row < rows; row++)
+	{
+		double div = equation[row][row];
+		for (size_t col = row; col < cols; col++)
+		{
+			equation[row][col] /= div;
+		}
+
+		for (size_t r = row + 1; r < rows; r++)
+		{
+			double mult = equation[r][row];
+			for (size_t col = row; col < cols; col++)
+			{
+				equation[r][col] -= equation[row][col] * mult;
+			}
+		}
+	}
+
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < cols; j++)
+		{
+			cout << equation[i][j] << ", ";
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
 	const int len = 5;
