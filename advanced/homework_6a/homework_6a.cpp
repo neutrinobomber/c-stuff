9672367678f297d6d9381dcb9478eb16755b2f5e
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2018-11-29 22:50:28 +0200

    added homework 6a

---
+++ advanced/homework_6a/homework_6a.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+typedef unsigned long ul;
+
+void PrintRotate(const string& data, ul rotations)
+{
+	ul index = rotations % data.size();
+	ul prints = data.size() > 10 ? 10 : data.size();
+	while (prints--)
+	{
+		cout << data[index++ % data.size()];
+	}
+}
+
+int main()
+{
+	string data = " ";
+	while (cin >> data)
+	{
+		string rotationsStr = "";
+
+		while (cin >> rotationsStr)
+		{
+			istringstream stream(rotationsStr);
+			ul rotations = 0;
+
+			if (stream >> rotations)
+			{
+				PrintRotate(data, rotations);
+				cout << endl;
+			}
+			else
+			{
+				data = rotationsStr;
+				continue;
+			}
+		}
+	}
+
+	return 0;
+}
