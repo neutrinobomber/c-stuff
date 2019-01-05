e1e0f96df6edbf5ada3a188293de30b55092d87c
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-01-05 14:58:09 +0200

    tsp done

--- advanced/homework_9b/homework_9b.cpp
+++ advanced/homework_9b/homework_9b.cpp
@@ -1,62 +1,148 @@
 ﻿#include "pch.h"
 
-#include <vector>
+// Александър Янков F87134
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <limits>
+#include <unordered_set>
 
 using namespace std;
-#define V 4
 
-int TravllingSalesmanProblem(int graph[][V], int startNode)
+typedef vector<vector<unsigned>> Graph;
+
+const unsigned MAX_VALUE = numeric_limits<unsigned>::max();
+
+unsigned minSum;
+unsigned curSum;
+
+vector<unsigned> used;
+vector<unsigned> cycle;
+vector<unsigned> minCycle;
+
+Graph graph;
+
+struct Edge
 {
-	// store all vertex apart from source vertex 
-	vector<int> nodes;
-	for (int currentNode = 0; currentNode < V; currentNode++)
+	unsigned from;
+	unsigned to;
+	unsigned weight;
+
+	Edge(unsigned from, unsigned to, unsigned weight) : 
+		from(from), to(to), weight(weight) {}
+};
+
+void PrintCycle(unsigned nodesCount)
+{
+	if (minSum == MAX_VALUE)
 	{
-		if (currentNode != startNode)
-		{
-			nodes.push_back(currentNode);
-		}
+		cout << "-1";
+		return;
 	}
 
-	// store minimum weight Hamiltonian Cycle. 
-	int minPath = INT_MAX;
-	do
+	cout << "1 ";
+	for (size_t i = 0; i < nodesCount - 1; i++)
 	{
-		// store current Path weight(cost) 
-		int currentPathWeight = 0;
+		cout << minCycle[i] + 1 << " ";
+	}
+}
 
-		// compute current path weight 
-		int currentNode = startNode;
-		int nextNode;
-		for (int nextIdx = 0; nextIdx < nodes.size(); nextIdx++) 
+void Hamilton(unsigned i, unsigned level)
+{
+	size_t k;
+	if ((0 == i) && (level > 0)) 
+	{
+		if (level == graph.size()) 
 		{
-			nextNode = nodes[nextIdx];
-			currentPathWeight += graph[currentNode][nextNode];
-			currentNode = nextNode;
+			minSum = curSum;
+			for (k = 0; k < graph.size(); k++)
+			{
+				minCycle[k] = cycle[k];
+			}
 		}
-		currentPathWeight += graph[currentNode][startNode];
+		return;
+	}
 
-		// update minimum 
-		minPath = min(minPath, currentPathWeight);
+	if (used[i])
+	{
+		return;
+	}
 
-	} while (next_permutation(nodes.begin(), nodes.end()));
+	used[i] = 1;
+	for (k = 0; k < graph.size(); k++)
+	{
+		if (graph[i][k] && k != i)
+		{
+			cycle[level] = k;
+			curSum += graph[i][k];
+			if (curSum < minSum)
+			{
+				Hamilton(k, level + 1);
+			}
+			curSum -= graph[i][k];
+		}
+	}
+	used[i] = 0;
+}
 
-	return minPath;
+void Reset()
+{
+	used = vector<unsigned>(graph.size());
+	fill(used.begin(), used.end(), 0);
+
+	cycle = vector<unsigned>(graph.size());
+	cycle[0] = 1;
+
+	minCycle = vector<unsigned>(graph.size());
+
+	minSum = MAX_VALUE;
+	curSum = 0;
+}
+
+void BuildGraph(Graph& graph, const vector<Edge>& edges, unsigned nodesCount)
+{
+	graph.resize(nodesCount);
+	for (size_t i = 0; i < graph.size(); i++)
+	{
+		graph[i].resize(nodesCount);
+		fill(graph[i].begin(), graph[i].end(), 0);
+	}
+
+	for (auto edge : edges)
+	{
+		graph[edge.from - 1][edge.to - 1] = edge.weight;
+		graph[edge.to - 1][edge.from - 1] = edge.weight;
+	}
 }
 
 int main()
 {
-	int graph[][V] = 
-	{ 
-		{ 0, 10, 15, 20 },
-		{ 10, 0, 35, 25 },
-		{ 15, 35, 0, 30 },
-		{ 20, 25, 30, 0 } 
-	};
+	unsigned edgesCount = 0;
+	while (cin >> edgesCount)
+	{
+		unordered_set<unsigned> nodes;
+		vector<Edge> edges;
 
-	int startNode = 0;
-	cout << TravllingSalesmanProblem(graph, startNode) << endl;
+		for (size_t i = 0; i < edgesCount; i++)
+		{
+			unsigned from = 0;
+			unsigned to = 0;
+			unsigned weight = 0;
+
+			cin >> from >> to >> weight;
+
+			nodes.insert(from);
+			nodes.insert(to);
+
+			edges.emplace_back(Edge(from, to, weight));
+		}
+
+		BuildGraph(graph, edges, nodes.size());
+		Reset();
+
+		Hamilton(0, 0);
+		PrintCycle(graph.size());
+		cout << endl;
+	}
 
 	return 0;
 }
